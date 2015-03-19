ac5bb9fedfd167c3d9d37d0b0a56f0cbc050354c
mszczeci <mszczeci@gmail.com>
2015-03-19 21:08:40 +0100

    Video capture template

--- eye_tracker/main.cpp
+++ eye_tracker/main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include "opencv2/opencv.hpp"
 
-int main(void)
+using namespace cv;
+
+int main(int, char**)
 {
-	system("pause");
+	VideoCapture cap(0); // open the default camera
+	if (!cap.isOpened())  // check if we succeeded
+		return -1;
+
+	for (;;)
+	{
+		Mat frame;
+		cap >> frame; // get a new frame from camera
+		if(!frame.empty()) imshow("display", frame);
+		if (waitKey(30) >= 0) break;
+	}
+	// the camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
 }
